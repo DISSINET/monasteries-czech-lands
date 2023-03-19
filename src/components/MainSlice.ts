d52fa00f3b67881aecb98ebaca70cacc07cbeb9f
Peter Ondrejka <pondrejk@redhat.com>
2023-03-19 20:25:50 +0100

    orders and statuses in state tree

--- src/components/MainSlice.ts
+++ src/components/MainSlice.ts
@@ -1,14 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface MainSlice {}
+export interface MainSlice {
+  selectedOrderLabels: Array<string>;
+  selectedStatusLabels: Array<string>;
+}
 
-const initialState: MainSlice = {};
+const initialState: MainSlice = {
+  selectedOrderLabels: [],
+  selectedStatusLabels: [],
+};
 
 export const mainSlice = createSlice({
   name: "layout",
   initialState,
-  reducers: {},
+  reducers: {
+    selectOrders: (state, action: PayloadAction<Array<string>>) => {
+      let newSelectedOrders = action.payload;
+      state.selectedOrderLabels = newSelectedOrders;
+    },
+    selectStatuses: (state, action: PayloadAction<Array<string>>) => {
+      let newSelectedStatuses = action.payload;
+      state.selectedStatusLabels = newSelectedStatuses;
+    },
+  },
 });
 
-export const {} = mainSlice.actions;
+export const { selectOrders, selectStatuses } = mainSlice.actions;
 export default mainSlice.reducer;
--- src/components/panel/Panel.tsx
+++ src/components/panel/Panel.tsx
@@ -15,6 +15,7 @@ import {
 import { DictOrders } from "../../shared/dictionaries/orders";
 import { DictStatuses } from "../../shared/dictionaries/statuses";
 import { BsCheckLg, BsListUl } from "react-icons/bs";
+import { selectOrders, selectStatuses } from "./../MainSlice";
 
 //import legend from "./../../assets/legend.png";
 
@@ -30,11 +31,77 @@ const PanelComponent = ({}: PanelComponentProps): JSX.Element => {
     ]
   );
 
+  const dispatch = useAppDispatch();
+
   const [infoModal, toggleInfoModal] = useState(false);
   const handleInfoModalClose = () => toggleInfoModal(false);
   const handleInfoModalShow = () => toggleInfoModal(true);
   const [showOrders, setShowOrders] = useState(false);
+  const handleShowOrders = () => setShowOrders(true);
   const [showStatuses, setShowStatuses] = useState(false);
+  const handleShowStatuses = () => setShowStatuses(true);
+
+  const selectedOrderLabels = useAppSelector(
+    (state) => state.main.selectedOrderLabels
+  );
+
+  const selectedStatusLabels = useAppSelector(
+    (state) => state.main.selectedStatusLabels
+  );
+
+  function selectOrder(selectedOrder: string) {
+    let selectedOrders = new Set(selectedOrderLabels);
+    if (selectedOrders.has(selectedOrder)) {
+      selectedOrders.delete(selectedOrder);
+    } else {
+      selectedOrders.add(selectedOrder);
+    }
+    dispatch(selectOrders(Array.from(selectedOrders)));
+  }
+
+  function selectStatus(selectedStatus: string) {
+    let selectedStatuses = new Set(selectedStatusLabels);
+    if (selectedStatuses.has(selectedStatus)) {
+      selectedStatuses.delete(selectedStatus);
+    } else {
+      selectedStatuses.add(selectedStatus);
+    }
+    dispatch(selectStatuses(Array.from(selectedStatuses)));
+  }
+
+  function clearOrders() {
+    dispatch(selectOrders([]));
+  }
+
+  function clearStatuses() {
+    dispatch(selectStatuses([]));
+  }
+
+  function filterControl(
+    label: string,
+    action: any = null,
+    stateArray: any = null,
+    clearAction: any = null
+  ) {
+    return (
+      <InputGroup size="sm">
+        <InputGroup.Text
+          onClick={action}
+          style={{ cursor: "pointer", flexGrow: 1 }}
+        >
+          <BsListUl style={{ marginRight: "6px" }} />
+          {label}
+        </InputGroup.Text>
+        {stateArray.length > 0 ? (
+          <Button variant="outline-primary" size="sm" onClick={clearAction}>
+            clear
+          </Button>
+        ) : (
+          ""
+        )}
+      </InputGroup>
+    );
+  }
 
   return (
     <div
@@ -57,31 +124,40 @@ const PanelComponent = ({}: PanelComponentProps): JSX.Element => {
         }}
       >
         <div id="section1">
-          <InputGroup size="sm">
-            <InputGroup.Text
-              style={{ cursor: "pointer", flexGrow: 1 }}
-              onClick={() => setShowOrders(true)}
-            >
-              <BsListUl style={{ marginRight: "6px" }} />
-              Order
-            </InputGroup.Text>
-          </InputGroup>
+          {filterControl(
+            "Order",
+            handleShowOrders,
+            selectedOrderLabels,
+            clearOrders
+          )}
           <Offcanvas
             show={showOrders}
             onHide={() => setShowOrders(false)}
             placement="end"
           >
             <Offcanvas.Header closeButton>
-              <b>Filter by Order</b>
+              {filterControl(
+                "Filter by order",
+                null,
+                selectedOrderLabels,
+                clearOrders
+              )}
             </Offcanvas.Header>
             <Offcanvas.Body>
               {DictOrders.map((e) => {
                 return (
-                  <Dropdown.Item id={e.id} className="ps-4">
+                  <Dropdown.Item
+                    id={e.id}
+                    className="ps-4 mt-2"
+                    onClick={() => selectOrder(e.value)}
+                  >
                     <>
                       <BsCheckLg
                         style={{
                           color: "blue",
+                          opacity: selectedOrderLabels.includes(e.value)
+                            ? 1
+                            : 0,
                         }}
                       />{" "}
                       {e.value}
@@ -94,40 +170,49 @@ const PanelComponent = ({}: PanelComponentProps): JSX.Element => {
         </div>
 
         <div id="section2">
-          <InputGroup size="sm">
-            <InputGroup.Text
-              style={{ cursor: "pointer", flexGrow: 1 }}
-              onClick={() => setShowStatuses(true)}
-            >
-              <BsListUl style={{ marginRight: "6px" }} />
-              Status
-            </InputGroup.Text>
-            <Offcanvas
-              show={showStatuses}
-              onHide={() => setShowStatuses(false)}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <b>Filter by Status</b>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                {DictStatuses.map((e) => {
-                  return (
-                    <Dropdown.Item id={e.id} className="ps-4">
-                      <>
-                        <BsCheckLg
-                          style={{
-                            color: "blue",
-                          }}
-                        />{" "}
-                        {e.value}
-                      </>
-                    </Dropdown.Item>
-                  );
-                })}
-              </Offcanvas.Body>
-            </Offcanvas>
-          </InputGroup>
+          {filterControl(
+            "Status",
+            handleShowStatuses,
+            selectedStatusLabels,
+            clearStatuses
+          )}
+          <Offcanvas
+            show={showStatuses}
+            onHide={() => setShowStatuses(false)}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              {filterControl(
+                "Filter by status",
+                handleShowStatuses,
+                selectedStatusLabels,
+                clearStatuses
+              )}
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              {DictStatuses.map((e) => {
+                return (
+                  <Dropdown.Item
+                    id={e.id}
+                    className="ps-4 m-2"
+                    onClick={() => selectStatus(e.value)}
+                  >
+                    <>
+                      <BsCheckLg
+                        style={{
+                          color: "blue",
+                          opacity: selectedStatusLabels.includes(e.value)
+                            ? 1
+                            : 0,
+                        }}
+                      />{" "}
+                      {e.value}
+                    </>
+                  </Dropdown.Item>
+                );
+              })}
+            </Offcanvas.Body>
+          </Offcanvas>
         </div>
 
         <div id="section3">
